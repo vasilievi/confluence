1290938d75a553c4f547a4b58d0632937f30872e
vasilievi <vasilievi@outlook.com>
2022-09-13 07:54:03 +0300

    init

--- src/index.js
+++ src/index.js
@@ -1,17 +1,47 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import MDEditor from "@uiw/react-md-editor";
+import 'bootstrap/dist/css/bootstrap.css';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const mkdStr = `
+# Компьютер Сервис
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+---
+## Картинка со ссылкой
+**New MD-editor**
+
+[![](https://cs-develop.ru/files/20220913/1663044003926_images.png)](https://cs-develop.ru)
+
+## Ссылки
+* [pcs.ru](https://pcs.ru)
+* [Enterprise](https://enterprise.cs-develop.ru/)
+
+## Код JavaScript
+\`\`\`javascript
+let message;
+message = 'Hello world!';
+
+alert(message);
+\`\`\`
+`;
+
+function App() {
+  const [value, setValue] = React.useState(mkdStr);
+  return (
+    <div className="container">
+      <button className='btn btn-success'>Test bootstrap</button>
+      <MDEditor
+        value={value}
+        onChange={setValue} />
+
+      <MDEditor.Markdown
+        style={{ padding: 15 }}
+        source={value}
+        linkTarget="_blank"
+      />
+    </div>
+  );
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
