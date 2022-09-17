992d9319a0ec7050ba8379651d89b2b417ebbdad
vasilievi <vasilievi@outlook.com>
2022-09-17 11:40:03 +0300

    -

--- src/index.js
+++ src/index.js
@@ -3,7 +3,8 @@ import ReactDOM from 'react-dom/client';
 import MDEditor from "@uiw/react-md-editor";
 import 'bootstrap/dist/css/bootstrap.css';
 
-let mkdStr = `
+function MarkDown(props) {
+  let mkdStr = `
 # Develop docs
 
 ---
@@ -24,17 +25,26 @@ message = 'Hello world!';
 alert(message);
 \`\`\`
 `;
+  const [value, setValue] = React.useState(mkdStr);
 
-// function EditButtons(props) {
-//   if (props.editMode) {
-//     return <button onClick={onEditButton} className='btn btn-success'>Edit</button>
-//   } else {
-//     return <button onClick={onSaveButton} className='btn btn-danger'>Save</button>
-//   }
+  if (props.editMode) {
+    return (
+      <MDEditor
+        height={500}
+        value={value}
+        onChange={setValue} />
+    )
+  }
 
+  return (
+    <MDEditor.Markdown
+      style={{ padding: 15 }}
+      source={value}
+      linkTarget="_blank"
+    />
+  );
 
-
-// }
+}
 
 class Page extends React.Component {
   constructor(props) {
@@ -57,7 +67,6 @@ class Page extends React.Component {
 
   render() {
     if (this.state.editMode) {
-
       return <div>
         <MarkDown editMode={this.state.editMode} />
         <button onClick={() => this.onSaveButton()} className='btn btn-danger'>Save</button>
@@ -69,45 +78,6 @@ class Page extends React.Component {
     </div>
   }
 }
-
-// function MarkDownEditor() {
-//   //const [value, setValue] = React.useState(mkdStr);
-//   return (
-//     <div className="container">
-//       <MDEditor.Markdown
-//         style={{ padding: 15 }}
-//         source={mkdStr}
-//         linkTarget="_blank"
-//       />
-
-//       <div className='mt-3 btn-group'>
-//         <EditButtons editMode={false} mdString={mkdStr}/>
-//       </div>
-//     </div>
-//   );
-// }
-
-//const [value] = React.useState(mkdStr);
-
-function MarkDown(props) {
-  const [value, setValue] = React.useState(mkdStr);
-
-  if (props.editMode) {
-    return (
-      <MDEditor height={500} value={value} onChange={setValue} />
-    )
-  }
-
-  return (
-    <MDEditor.Markdown
-      style={{ padding: 15 }}
-      source={value}
-      linkTarget="_blank"
-    />
-  );
-
-}
-
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<Page />);
