e7ebbd3569cef8f64288b44d667b9bad8a3bdcd1
vasilievi <vasilievi@outlook.com>
2022-09-17 07:54:44 +0300

    -

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import MDEditor from "@uiw/react-md-editor";
 import 'bootstrap/dist/css/bootstrap.css';
 
-const mkdStr = `
+let mkdStr = `
 # Develop docs
 
 ---
@@ -25,33 +25,89 @@ alert(message);
 \`\`\`
 `;
 
-let editMode = true
+// function EditButtons(props) {
+//   if (props.editMode) {
+//     return <button onClick={onEditButton} className='btn btn-success'>Edit</button>
+//   } else {
+//     return <button onClick={onSaveButton} className='btn btn-danger'>Save</button>
+//   }
 
-function EditButtons(props) {
-  if (props.editMode) {
-    return <button className='btn btn-success'>Edit</button>
-  } else {
-    return <button className='btn btn-danger'>Save</button>
+
+
+// }
+
+class Page extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      editMode: props.editMode,
+      mdString: props.mdString,
+    };
+  }
+
+  onEditButton() {
+    console.log('onEditButton');
+    this.setState({ editMode: true });
+  }
+
+  onSaveButton() {
+    console.log('onSaveButton');
+    this.setState({ editMode: false });
+  }
+
+  render() {
+    if (this.state.editMode) {
+
+      return <div>
+        <MarkDown editMode={this.state.editMode} />
+        <button onClick={() => this.onSaveButton()} className='btn btn-danger'>Save</button>
+      </div>
+    }
+    return <div>
+      <MarkDown editMode={this.state.editMode} />
+      <button onClick={() => this.onEditButton()} className='btn btn-success'>Edit</button>
+    </div>
   }
 }
 
-function MarkDownEditor() {
+// function MarkDownEditor() {
+//   //const [value, setValue] = React.useState(mkdStr);
+//   return (
+//     <div className="container">
+//       <MDEditor.Markdown
+//         style={{ padding: 15 }}
+//         source={mkdStr}
+//         linkTarget="_blank"
+//       />
+
+//       <div className='mt-3 btn-group'>
+//         <EditButtons editMode={false} mdString={mkdStr}/>
+//       </div>
+//     </div>
+//   );
+// }
+
+//const [value] = React.useState(mkdStr);
+
+function MarkDown(props) {
   const [value, setValue] = React.useState(mkdStr);
+
+  if (props.editMode) {
+    return (
+      <MDEditor height={500} value={value} onChange={setValue} />
+    )
+  }
+
   return (
-    <div className="container">
-      <MDEditor.Markdown
-        style={{ padding: 15 }}
-        source={value}
-        linkTarget="_blank"
-      />
-      <MDEditor
-        value={value}
-        onChange={setValue} />
-      <div className='mt-3 btn-group'></div>
-      <EditButtons editMode = {editMode} />
-    </div>
+    <MDEditor.Markdown
+      style={{ padding: 15 }}
+      source={value}
+      linkTarget="_blank"
+    />
   );
+
 }
 
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<MarkDownEditor />);
+root.render(<Page />);
