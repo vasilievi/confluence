c3a13f57038335a056988113731326fcfb0eee5e
vasilievi <vasilievi@outlook.com>
2022-09-18 10:13:10 +0300

    -

--- src/ArticleEdit.js
+++ src/ArticleEdit.js
@@ -1,27 +1,26 @@
-import {useEffect, useState} from 'react';
+import { useEffect, useState } from 'react';
 import {
     useParams
 } from "react-router-dom";
 import MDEditor from "@uiw/react-md-editor";
 
 export default function ArticleView() {
-    const [articleJson, setarticleJson] = useState({text: "init text"});
+    const [text, setText] = useState('# init text');
     let { path } = useParams();
 
     useEffect(() => {
         fetch(`https://confluence.cs-develop.ru/getArticle/${path}`)
             .then(response => response.json())
-            .then(setarticleJson);
+            .then((resJson) => setText(resJson.text));
     }, []);
 
     return (
         <div>
-            <h3>View Article : {path}</h3>
-            <MDEditor.Markdown
-                style={{ padding: 15 }}
-                source={articleJson.text}
-                linkTarget="_blank"
-            />
+            <h3>Edit Article : {path}</h3>
+            <MDEditor
+                height={500}
+                value={text}
+                onChange={setText} />
         </div>
     );
 
--- src/Router.js
+++ src/Router.js
@@ -6,6 +6,7 @@ import {
 } from "react-router-dom";
 
 import ArticleView from "./ArticleView"
+import ArticleEdit from "./ArticleEdit"
 
 export default function App() {
   return (
@@ -31,13 +32,13 @@ const Home = () => <h1>Welcome to confluence!</h1>;
 //   );
 // }
 
-const ArticleEdit = () => {
-  let { path } = useParams();
+// const ArticleEdit = () => {
+//   let { path } = useParams();
 
-  return (
-    <div>
-      <h3>Edit Article : {path}</h3>
-    </div>
-  );
-}
+//   return (
+//     <div>
+//       <h3>Edit Article : {path}</h3>
+//     </div>
+//   );
+// }
 
