e810a4f256ae963742cb354d0c76f551bad74d6a
vasilievi <vasilievi@outlook.com>
2022-09-18 08:01:37 +0300

    -

--- src/index.js
+++ src/index.js
@@ -1,12 +1,5 @@
-import { StrictMode } from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from 'react-dom/client';
+import Router from "./Router";
 
-import App from "./App";
-
-const rootElement = document.getElementById("root");
-ReactDOM.render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-  rootElement
-);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<Router />);
