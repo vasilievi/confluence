5e3eb8bc0115531ad455dc769cf9a4abed18279e
vasilievi <vasilievi@outlook.com>
2022-09-19 07:33:54 +0300

    -

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import ReactDOM from 'react-dom/client';
 import Router from "./Router";
+import 'bootstrap/dist/css/bootstrap.css';
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<Router />);
