beaa891c758976237413bbc861cb1a29b8a4d9e2
vasilievi <vasilievi@outlook.com>
2022-11-19 17:29:11 +0300

    -

--- src/ArticleEdit.js
+++ src/ArticleEdit.js
@@ -10,6 +10,7 @@ export default function ArticleView() {
     const [text, setText] = useState('## loading ...');
     const [articleState, setArticleState] = useState('unblocked');
     const [modified, setModified] = useState(false);
+    const [screenHeight, setScreenHeight] = useState(500);
     let { path } = useParams();
 
     const socket = io(process.env.REACT_APP_HOST);
@@ -29,8 +30,8 @@ export default function ArticleView() {
         console.log("unBlockedArticle " + unBlockedArticlePath);
         if (path === unBlockedArticlePath) {
             await fetch(`${process.env.REACT_APP_HOST}/getArticle/${path}`)
-            .then(response => response.json())
-            .then((resJson) => setText(resJson.text));
+                .then(response => response.json())
+                .then((resJson) => setText(resJson.text));
 
             setArticleState('unblocked')
         }
@@ -39,9 +40,11 @@ export default function ArticleView() {
     document.title = 'Edit mode: ' + path
 
     useEffect(() => {
+        console.log('useEffect');
         fetch(`${process.env.REACT_APP_HOST}/getArticle/${path}`)
             .then(response => response.json())
             .then((resJson) => setText(resJson.text));
+        setScreenHeight(window.screen.height - 250)
     }, [path]);
 
     function onChangeText(newText) {
@@ -52,27 +55,66 @@ export default function ArticleView() {
         setText(newText)
     }
 
+    const snackbar = (text) => {
+        let snackbarElement = document.getElementById("snackbar");
+        snackbarElement.innerText = text
+        snackbarElement.className = "show";
+        setTimeout(function(){ snackbarElement.className = snackbarElement.className.replace("show", ""); }, 3000);
+    }
+
     function SaveButton() {
+        async function onSaveButtonClick() {
+            console.log('onSaveButtonClick');
+            let res = await fetch(`${process.env.REACT_APP_HOST}/postArticle/${path}`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({ text: text })
+            });
+            let resJson = await res.json()
+            if (resJson) {
+                if (resJson.success) {
+                    setModified(false)
+                    socket.emit('savedArticle', path);
+                }
+            }
+            console.log(resJson);
+        }
+
         if (modified) return <button className='btn btn-warning' onClick={onSaveButtonClick}>Save</button>
     }
 
-    async function onSaveButtonClick() {
-        console.log('onSaveButtonClick');
-        let res = await fetch(`${process.env.REACT_APP_HOST}/postArticle/${path}`, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({ text: text })
-        });
-        let resJson = await res.json()
-        if (resJson) {
-            if (resJson.success) {
-                setModified(false)
-                socket.emit('savedArticle', path);
+    function UploadButton() {
+
+        function onClickUploadButton() {
+            console.log('onClickUploadFile');
+            document.getElementById("inputFile").click();
+        }
+
+        async function onChangeInputFile() {
+            let formData = new FormData();
+            formData.append("filedata", window.event.target.files[0]);
+            let res = await fetch("https://cs-develop.ru/upload", {
+                method: "POST",
+                body: formData,
+            });
+
+            if (res.status === 200) {
+                let resText = await res.text();
+                console.log(resText);
+
+                navigator.clipboard.writeText(resText);
+                snackbar('Ссылка скопирована в буфер обмена!')
             }
         }
-        console.log(resJson);
+
+        if (modified) return (
+            <div>
+                <input type="file" id="inputFile" style={{ display: "none" }} onChange={onChangeInputFile} />
+                <button className='btn btn-primary' onClick={onClickUploadButton}>Upload file</button>
+            </div>
+        )
     }
 
     function onViewModeClick() {
@@ -82,16 +124,23 @@ export default function ArticleView() {
 
     return (
         <div>
-            <div className='btn-group m-3'>
-                <button className='btn btn-outline-success' onClick={onViewModeClick}>View mode</button>
-                <SaveButton />
-                <button className='btn btn-outline-secondary' disabled>{articleState}</button>
+            <div className='row m-3'>
+                <div className='col-9 btn-group'>
+                    <button className='btn btn-outline-success' onClick={onViewModeClick}>View mode</button>
+                    <SaveButton />
+                    <button className='btn btn-outline-secondary' disabled>{articleState}</button>
+                </div>
+                <div className='col-3'>
+                    <UploadButton className='ms-1' />
+                </div>
             </div>
-            <hr></hr>
+
             <MDEditor
-                height={500}
+                height={screenHeight}
                 value={text}
                 onChange={onChangeText} />
+
+            <div id="snackbar">Some text some message..</div>
         </div>
     );
 
